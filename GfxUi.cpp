665b3cef92537f87232c1f9cbccd15c421369ef7
Kamjon6.11 <98246079+CatDBB101@users.noreply.github.com>
2025-03-05 07:27:54 +0700

    lib

--- GfxUi.cpp
+++ GfxUi.cpp
@@ -1,14 +1,23 @@
 #include <Arduino.h>
 #include <GfxUi.h>
 
-GfxUi::GfxUi(Arduino_GFX *_gfx) {
+GfxUi::GfxUi(Arduino_GFX* _gfx, int _fontWidth, int _fontHeight) {
   gfx = _gfx;
+  fontWidth = _fontWidth;
+  fontHeight = _fontHeight;
 }
 
-void GfxUi::begin() {
-  Serial.println("Hello, World!");
-  // gfx->setTextColor(0xFFFF);
-  gfx->setTextSize(2);
-  gfx->setCursor(20, 20);
-  gfx->println(String("Hello, World!"));
+void GfxUi::drawCanva(
+  int16_t x, int16_t y,
+  int16_t w, int16_t h,
+  int16_t r, uint16_t c) {
+  gfx->drawRoundRect(x, y, w, h, r, c);
+}
+
+int GfxUi::strWidth(const char* s) {
+  return strlen(s) * fontWidth;
+}
+
+int GfxUi::strHeight(const char* s) {
+  return strlen(s) * fontHeight;
 }
--- GfxUi.h
+++ GfxUi.h
@@ -2,8 +2,12 @@
 
 class GfxUi {
 public:
-  GfxUi(Arduino_GFX *gfx);
-  void begin();
+  GfxUi(Arduino_GFX* gfx, int fontWidth, int fontHeight);
+  void drawCanva(int16_t x, int16_t y, int16_t w, int16_t h, int16_t r, uint16_t c);
+  int strWidth(const char* s);
+  int strHeight(const char* s);
 private:
-  Arduino_GFX *gfx;
+  Arduino_GFX* gfx;
+  int fontWidth;
+  int fontHeight;
 };
